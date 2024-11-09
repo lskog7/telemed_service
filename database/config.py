127387edf3caa52fd79d2ea8b223512b5f8176b8
Терещенко Артемий Владимирович <tereschenkoav3@zdrav.mos.ru>
2024-11-10 00:54:24 +0300

    added basic tables + alembic + migrations (testing)

--- database/config.py
+++ database/config.py
@@ -3,12 +3,12 @@ from pydantic_settings import BaseSettings, SettingsConfigDict
 
 
 class Settings(BaseSettings):
-    DB_USER: str
-    DB_PASSWORD: str
-    DB_HOST: str
-    DB_PORT: int
-    DB_NAME: str
-    DATABASE_SQLITE: str = 'sqlite+aiosqlite:///data/db.sqlite3'
+    # DB_USER: str
+    # DB_PASSWORD: str
+    # DB_HOST: str
+    # DB_PORT: int
+    # DB_NAME: str
+    DATABASE_SQLITE: str = 'sqlite+aiosqlite:///database/data/db.sqlite3'
 
     model_config = SettingsConfigDict(
         env_file=os.path.join(os.path.dirname(os.path.abspath(__file__)), ".env")
--- database/models.py
+++ database/models.py
@@ -1,5 +1,5 @@
 from sqlalchemy import ForeignKey, text, ARRAY, JSON, String, Text
-from sqlalchemy.orm import Mapped, mapped_column
+from sqlalchemy.orm import Mapped, mapped_column, relationship
 from typing import List
 
 from database.database import Base, uniq_str_an
@@ -12,6 +12,26 @@ class User(Base):
     password: Mapped[str]
     profile_id: Mapped[int | None] = mapped_column(ForeignKey("profiles.id"))
 
+    # One-to-one with Profile
+    profile: Mapped["Profile"] = relationship(
+        "Profile",
+        back_populates="user",
+        uselist=False,  # Ключевой параметр для связи один-к-одному
+        lazy="joined"  # Автоматически подгружает profile при запросе user
+    )
+    # One-to-many with Posts
+    posts: Mapped[list["Post"]] = relationship(
+        "Post",
+        back_populates="user",
+        cascade="all, delete-orphan"  # Удаляет посты при удалении пользователя
+    )
+    # One-to-many with Comments
+    comments: Mapped[list["Comment"]] = relationship(
+        "Comment",
+        back_populates="user",
+        cascade="all, delete-orphan"  # При удалении User удаляются и связанные Comment
+    )
+
 
 class Profile(Base):
     first_name: Mapped[str]
@@ -20,24 +40,54 @@ class Profile(Base):
     gender: Mapped[GenderEnum]
     profession: Mapped[ProfessionEnum] = mapped_column(
         default=ProfessionEnum.DEVELOPER,
-        server_default=text("UNEMPLOYED")
+        # server_default=text("UNEMPLOYED")
     )
-    interests: Mapped[List[str] | None] = mapped_column(ARRAY(String))
+    interests: Mapped[List[str] | None] = mapped_column(JSON)
     contacts: Mapped[dict | None] = mapped_column(JSON)
+
+    # Back One-to-one with User
+    user: Mapped["User"] = relationship(
+        "User",
+        back_populates="profile",
+        uselist=False
+    )
 
 
 class Post(Base):
     title: Mapped[str]
-    content: Mapped[Text]
+    content: Mapped[str] =  mapped_column(Text)
     main_photo_url: Mapped[str]
-    photos_url: Mapped[List[str] | None] = mapped_column(ARRAY(String))
+    photos_url: Mapped[List[str] | None] = mapped_column(JSON)
     status: Mapped[StatusPostEnum] = mapped_column(default=StatusPostEnum.PUBLISHED, server_default=text("'DRAFT'"))
     user_id: Mapped[int] = mapped_column(ForeignKey('users.id'))
 
+    # Many-to-one with User
+    user: Mapped["User"] = relationship(
+        "User",
+        back_populates="posts"
+    )
+    # One-to-many with Comments
+    comments: Mapped[list["Comment"]] = relationship(
+        "Comment",
+        back_populates="post",
+        cascade="all, delete-orphan"
+    )
+
 
 class Comment(Base):
-    content: Mapped[Text]
+    content: Mapped[str] = mapped_column(Text)
     user_id: Mapped[int] = mapped_column(ForeignKey('users.id'))
     post_id: Mapped[int] = mapped_column(ForeignKey('posts.id'))
-    is_published: Mapped[bool] = mapped_column(default=True, server_default=text("'false'"))
+    is_published: Mapped[bool] =  mapped_column(default=True, server_default=text("'false'"))
     rating: Mapped[RatingEnum] = mapped_column(default=RatingEnum.FIVE, server_default=text("'SEVEN'"))
+
+    # Many-to-one with Post
+    post: Mapped["Post"] = relationship(
+        "Post",
+        back_populates="comments",
+    )
+    # Many-to-one with User
+    user: Mapped["User"] = relationship(
+        "User",
+        back_populates="comments",
+    )
